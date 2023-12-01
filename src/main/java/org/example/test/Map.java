8dd9bed3f832dfa0df096b3b893dca40cdab7787
Dan <baitursonov@gmail.com>
2023-12-01 22:14:52 +0600

    added hw4

---
+++ src/main/java/org/example/test/Map.java
@@ -0,0 +1,11 @@
+package org.example.test;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Map extends JPanel {
+    Map(){
+        setBackground(Color.BLACK);
+    }
+    void startNewGame(int mode, )
+}
